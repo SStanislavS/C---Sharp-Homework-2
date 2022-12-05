ac9e9534b06927266bc745aed4e4d4123a4e0536
Stanislav <S@mail.ru>
2022-12-05 15:52:46 +0300

    add Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Программа на входе принимает трехзначное число, а на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+string N = Console.ReadLine();
+Console.Write("Вторая цифра: ");
+Console.WriteLine(N[N.Length - 2]);
