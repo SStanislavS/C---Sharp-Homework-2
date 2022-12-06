32aab324c8d1d58f5dca36699009df6e2793ef35
Stanislav <S@mail.ru>
2022-12-06 19:54:19 +0300

    add Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа на входе принимает число, а на выходе показываает третью цифру заданного числа или сообщает, что цифры нет.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 99)
+{
+    Console.WriteLine("Третьей цифры нет: ");
+}
+if (N > 99)
+{
+    Console.Write("Третья цифра: ");
+    Console.WriteLine(N.ToString()[2]);
+}
